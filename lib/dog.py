3b2165414eb859123b21ddc96bc5ded21287e4ed
josphat <mjirura0707@gmail.com>
2023-08-22 11:53:44 +0300

    completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+
+dog1 = Dog("Buddy")
+dog2 = Dog("Max", "Golden Retriever")
+
+
+print(dog1.name) 
+print(dog1.breed)
+
+print(dog2.name)
+print(dog2.breed)
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+
+person1 = Person("Guido")
+person2 = Person("Bob")
+
+
+print(person1.name) 
+print(person2.name)
